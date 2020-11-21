bcb4a5e889238243429f8c143573b4c1df37b33b
Marco Schumacher <marco.schumacher@pentlandfirth.com>
2020-11-21 13:24:24 +0100

    wip

--- src/state/cache.ts
+++ src/state/cache.ts
@@ -1,37 +1,43 @@
-import { enableMapSet } from 'immer';
+import { Draft, enableMapSet } from 'immer';
 import hash from 'object-hash';
 import { useEffect } from 'react';
 import { Store, useStoreState } from './state';
 
 enableMapSet();
 
-export class Cache<T extends Record<string, any>, Id> extends Store<Map<Id, T>> {
-  id: (t: T) => Id;
+type KeysOfStringProps<T> = keyof { [K in keyof T as T[K] extends string ? K : never]: T[K] };
 
-  constructor(id: (Id extends keyof T ? Id : never) | ((t: T) => Id)) {
-    super(new Map());
-    this.id = id instanceof Function ? id : (t) => t[id];
+export class Cache<T extends Record<string, any>> extends Store<{ [id: string]: T }> {
+  id: (t: T) => string;
+
+  constructor(id: KeysOfStringProps<T> | ((t: T) => string)) {
+    super({});
+    this.id = id instanceof Function ? id : (t) => t[id as keyof T];
   }
 
-  createAction<A extends (...args: any[]) => T | T[] | Promise<T> | Promise<T[]>>(action: A): Action<T, Id, A> {
+  createAction<A extends (...args: any[]) => T | T[] | Promise<T> | Promise<T[]>>(action: A): Action<T, A> {
     return new Action(this, action);
   }
 }
 
-export class Action<T extends Record<string, any>, Id, A extends (...args: any[]) => T | T[] | Promise<T> | Promise<T[]>> {
-  ids = new Store<Map<string, Id | Id[]>>(new Map());
+export class Action<T extends Record<string, any>, A extends (...args: any[]) => T | T[] | Promise<T> | Promise<T[]>> {
+  ids = new Store<{ [key: string]: string | string[] }>({});
   isLoading = new Set<string>();
 
-  constructor(public cache: Cache<T, Id>, private action: A) {}
+  constructor(public cache: Cache<T>, private action: A) {}
 
   use(...args: Parameters<A>) {
     const key = hash(args);
-    const ids = useStoreState(this.ids, (ids) => ids.get(key), [key]);
+    const ids = useStoreState(this.ids, (ids) => ids[key], [key]);
     const items = useStoreState(
       this.cache,
       (items) => {
-        if (ids instanceof Array) return ids.map((id) => items.get(id)!);
-        if (ids) return items.get(ids)!;
+        if (ids instanceof Array) {
+          const mapped = ids.map((id) => items[id]);
+          if (mapped.some((item) => item === undefined)) return undefined;
+          return mapped as T[];
+        }
+        if (ids) return items[ids];
         return undefined;
       },
       [ids]
@@ -53,11 +59,11 @@ export class Action<T extends Record<string, any>, Id, A extends (...args: any[]
       for (const item of result instanceof Array ? result : [result]) {
         this.cache.update((state) => {
           const id = this.cache.id(item);
-          state.set(id as any, item as any);
+          state[id] = item as Draft<T>;
         });
         const ids = result instanceof Array ? result.map((item) => this.cache.id(item)) : this.cache.id(result);
         this.ids.update((state) => {
-          state.set(key, ids as any);
+          state[key] = ids;
         });
       }
     } catch (e) {
