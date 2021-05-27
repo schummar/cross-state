8d946b1f0572ada8aeedd15e87efeab1b08589f7
Marco Schumacher <marco@schumacher.dev>
2021-05-27 10:31:54 +0200

    Fixed hash function.

--- src/hash.ts
+++ src/hash.ts
@@ -1,6 +1,6 @@
 export function hash(arg: unknown): string {
   return JSON.stringify({ arg }, (_key, value) => {
-    if (!(value instanceof Array) && typeof value === 'object' && arg !== null) {
+    if (value instanceof Object && !(value instanceof Array)) {
       const obj: any = {};
       for (const key of Object.keys(value).sort()) {
         obj[key] = value[key as keyof typeof value];
