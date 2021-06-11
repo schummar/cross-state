9db188190898930fafb00a34cea7da5766f1595b
Marco Schumacher <marco@schumacher.dev>
2021-06-12 01:14:57 +0200

    Don't use useEqualityRef for useState deps.

--- src/react/index.ts
+++ src/react/index.ts
@@ -4,4 +4,5 @@ export * from './action';
 export * from './store';
 export * from './storeScope';
 export * from './useAction';
+export * from './useEqualityRef';
 export * from './useStoreState';
--- src/react/useStoreState.ts
+++ src/react/useStoreState.ts
@@ -1,7 +1,6 @@
 import { useEffect, useMemo, useState } from 'react';
 import { createSelector, SelectorPaths, SelectorValue } from '../helpers/stringSelector';
 import { Store } from './store';
-import useEqualityRef from './useEqualityRef';
 
 export type UseStoreStateOptions = { throttle?: number };
 
@@ -29,17 +28,15 @@ export function useStoreState<T, S>(store: Store<T>, ...args: any[]): S {
     options = args[0] ?? {};
   }
 
-  // This value changes when deps change according to fast-deep-equal
-  const depsRef = useEqualityRef(deps);
   // This counter is incremented when the store notifies about changes, in order to trigger another render
   const [counter, setCounter] = useState(0);
   // This value therefore updates when either the store or the deps change or the store notifies
-  const value = useMemo(() => selector(store.getState()), [store, depsRef, counter]);
+  const value = useMemo(() => selector(store.getState()), [store, counter, ...deps]);
 
   // The subscription is setup on first render and when store or deps change.
   // The third parameter of subscribe means that it emit and update right after subscription. I think this is important
   // because between evaluating in useMemo and running the effect some time passes, so we can't be sure the value hasn't changed in between.
-  useEffect(() => store.subscribe(selector, () => setCounter((c) => c + 1), { throttle: options.throttle }), [store, depsRef]);
+  useEffect(() => store.subscribe(selector, () => setCounter((c) => c + 1), { throttle: options.throttle }), [store, ...deps]);
 
   return value;
 }
