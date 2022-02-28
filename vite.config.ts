7ea3ba24fb2018985caa1254082d55eaae8ccf62
Marco Schumacher <marco@schumacher.dev>
2022-02-28 21:50:58 +0100

    better build

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 /// <reference types="vitest" />
+import { isAbsolute } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
@@ -6,5 +7,26 @@ export default defineConfig({
     environment: 'jsdom',
     open: true,
     setupFiles: ['./test/_setup.ts'],
+  },
+
+  build: {
+    sourcemap: true,
+    minify: false,
+
+    lib: {
+      entry: 'src/index.ts',
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      input: {
+        index: 'src/index.ts',
+        react: 'src/react/index.ts',
+      },
+      output: {
+        entryFileNames: '[format]/[name].js',
+        chunkFileNames: '[format]/[name].js',
+      },
+      external: (source) => !(isAbsolute(source) || source.startsWith('.')),
+    },
   },
 });
