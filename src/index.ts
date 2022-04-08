38fc2b1739a03ead26a61576b71c6268c81b81e9
Marco Schumacher <marco@schumacher.dev>
2022-04-08 20:49:53 +0200

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 export { asyncStore } from './core/asyncStore';
-export type { AsyncAction, AsyncCollection, AsyncStore, AsyncStoreOptions, AsyncStoreValue, Time } from './core/asyncStore';
+export type { AsyncAction, AsyncCollection, AsyncStore, AsyncStoreOptions, AsyncStoreValue } from './core/asyncStore';
 export { computed } from './core/computed';
 export { once } from './core/once';
 export { store } from './core/store';
 export type { BoundStoreActions, StoreActions } from './core/store';
 export { arrayActions, mapActions, recordActions, setActions } from './core/storeActions';
+export { calcTime } from './lib/calcTime';
 export { persist } from './persist/persist';
 export type { PersistOptions } from './persist/persistOptions';
 export type { PersistPath } from './persist/persistPath';
 export type { PersistStorage } from './persist/persistStorage';
-export type { Cancel, Listener, Store, SubscribeOptions } from './types';
+export type { BaseStore, Cancel, Effect, Listener, Store, SubscribeOptions, Time } from './types';
