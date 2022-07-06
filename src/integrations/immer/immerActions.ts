b2e4b76c84b3c6d53b820d51bf59927fc991b819
Marco Schumacher <marco@schumacher.dev>
2022-07-06 17:16:15 +0200

    wip

--- src/integrations/immer/immerActions.ts
+++ src/integrations/immer/immerActions.ts
@@ -1,9 +1,13 @@
 import type { Draft } from 'immer';
 import { produce } from 'immer';
+import type { Nothing } from 'immer/dist/internal';
 import type { AtomicStore } from '../../core/atomicStore';
 
 export const immerActions = {
-  update<T>(this: AtomicStore<T>, recipe: (draft: Draft<T>) => Draft<T> | void | undefined) {
+  update<T>(
+    this: AtomicStore<T>,
+    recipe: (draft: Draft<T>) => void | Draft<T> | (Draft<T> extends undefined ? Nothing : never) | undefined
+  ) {
     this.set((value) => produce(value, recipe));
   },
 };
