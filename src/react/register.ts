5a405b3629eb7c5371c225cb153a1e25c6cac352
Marco Schumacher <marco@schumacher.dev>
2023-04-06 15:04:18 +0200

    wip

--- src/react/register.ts
+++ src/react/register.ts
@@ -1,11 +1,23 @@
 import { reactMethods } from './reactMethods';
-import { Store } from '@core';
+import { type UseCacheOptions, useCache } from './useCache';
+import { Cache, Store } from '@core';
 
-type ReactMethods = typeof reactMethods;
+type StoreMethods = typeof reactMethods;
+type CacheMethods = typeof cacheMethods;
 
 declare module '@core' {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  interface Store<T> extends ReactMethods {}
+  interface Store<T> extends StoreMethods {}
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  interface Cache<T> extends CacheMethods {}
 }
 
+const cacheMethods = {
+  useCache<T>(this: Cache<T>, options?: UseCacheOptions) {
+    return useCache(this, options);
+  },
+};
+
 Object.assign(Store.prototype, reactMethods);
+Object.assign(Cache.prototype, cacheMethods);
