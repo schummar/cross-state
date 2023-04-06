b649f7426c7a2cf02093d128c10923ccbbcb6709
Marco Schumacher <marco@schumacher.dev>
2023-04-06 16:08:23 +0200

    wip

--- src/core/scope.ts
+++ src/core/scope.ts
@@ -1,5 +1,9 @@
 export class Scope<T> {
-  constructor(public readonly defaultValue: T) {}
+  constructor(public readonly defaultValue: T) {
+    if ('Provider' in this && this.Provider instanceof Function) {
+      this.Provider = this.Provider.bind(this) as any;
+    }
+  }
 }
 
 export function createScope<T>(defaultValue: T): Scope<T> {
