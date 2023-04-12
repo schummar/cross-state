4c75e2e7a7c094db7d68dce4eef41c8fa72247ac
Marco Schumacher <marco@schumacher.dev>
2023-04-12 11:43:49 +0200

    feat: expose path helpers

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export * from './core';
 export { calcDuration } from './lib/calcDuration';
 export { InstanceCache } from './lib/instanceCache';
+export { type Path, type PathAsArray, type PathAsString, type Value } from './lib/path';
+export { get, set } from './lib/propAccess';
 export { arrayMethods, mapMethods, recordMethods, setMethods } from './lib/standardMethods';
 export * from './lib/updateHelpers';
 export * from './persist';
