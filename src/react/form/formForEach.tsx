ad873b7ccbf41c35431ea1dfe7e1c2d384928fee
Marco Schumacher <marco@schumacher.dev>
2023-09-21 11:20:15 +0200

    fix: slow ForEachPath

--- src/react/form/formForEach.tsx
+++ src/react/form/formForEach.tsx
@@ -1,15 +1,8 @@
 import { type GetKeys, type Join, type PathAsString, type Value } from '@lib/path';
-import type { Array_, Object_ } from '@lib/typeHelpers';
 import { Fragment, useCallback, type ReactNode } from 'react';
 import { type FieldHelperMethods, type Form } from './form';
 
-export type ForEachPath<T> = keyof {
-  [P in PathAsString<T> as NonNullable<Value<T, P>> extends readonly any[] | Record<string, any>
-    ? P
-    : never]: never;
-} &
-  PathAsString<T> &
-  string;
+export type ForEachPath<T> = PathAsString<T>;
 
 export type ElementName<TDraft, TPath extends PathAsString<TDraft>> = Join<
   TPath,
