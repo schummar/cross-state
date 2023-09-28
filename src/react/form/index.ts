672e1cc7ac46f96593c6f7ab60a80a22c801e1f1
Marco Schumacher <marco@schumacher.dev>
2023-09-28 15:56:12 +0200

    feat: more form types

--- src/react/form/index.ts
+++ src/react/form/index.ts
@@ -1,10 +1,15 @@
 export {
-  type Field,
   Form,
+  createForm,
+  type Field,
+  type FormContext,
+  type FormDerivedState,
+  type FormInstance,
   type FormOptions,
+  type FormState,
+  type Transform,
   type Validation,
   type Validations,
-  createForm,
 } from './form';
 export { type FormErrorProps } from './formError';
 export { type FormFieldComponent, type FormFieldProps } from './formField';
